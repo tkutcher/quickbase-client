668b8f78f9b81f6edca3c8640f4b1a825ae0eebc
Sam Nelson <sanelson@siliconfuture.net>
2023-07-27 13:33:10 -0600

    Added test to check for make_request return #63

--- tests/test_client/test_legacy_client.py
+++ tests/test_client/test_legacy_client.py
@@ -33,3 +33,20 @@ class TestQuickbaseLegacyApiClient:
         assert kwargs["url"] == "https://foo.quickbase.com/db/abc"
         assert "<rid>6" in kwargs["data"]
         assert "<newowner>tkutcher" in kwargs["data"]
+
+    def test_make_request_returns_response(self, mocker):
+        client = QuickbaseLegacyApiClient("whocares", "foo.quickbase.com")
+        request_mock = mocker.patch.object(client.session, "request")
+        mock_xml_response = """
+            <?xml version="1.0" ?>
+            <qdbapi>
+                <action>API_FooTest</action>
+            </qdbapi>
+        """
+        request_mock.return_value.text = mock_xml_response
+
+        response = client.make_request(
+            "POST", "API_FooTest", endpoint="/db/foo", request_data_xml_str=""
+        )
+        assert response.text == mock_xml_response
+        assert request_mock.call_count == 1
